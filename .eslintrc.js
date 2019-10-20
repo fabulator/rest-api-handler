85f9fb6303020dea9c72b7978b8dc251fa87172a
Michal Ozogán <michal@ozogan.eu>
2019-10-20 21:47:23 +0200

    Test: Github actions

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,8 @@ module.exports = {
     extends: [
         '@socifi',
     ],
+    rules: {
+        '@typescript-eslint/ban-ts-ignore': 0,
+        'unicorn/consistent-function-scoping': 0,
+    }
 };
